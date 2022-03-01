dba0e61f7add5b8b2848a6ed59e440b1b9c92808
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-01 19:40:38 -0300

    Cria formulário básico com campos título e autor com uma pequena estilização.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+
+import { useState } from 'react';
+import './style.css';
+
 import firebase from "./firebaseConnection";
 
 function App() {
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  async function handleAdd(){
+    
+    await firebase.firestore().collection('posts')
+    .add({
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(()=>{
+      console.log('DADOS CADASTRADO COM SUCESSO!');
+      setTitulo('');
+      setAutor('');
+    })
+    .catch((error)=>{
+      console.log('GEROU ALGUM ERRO: ' + error);
+    })
+
+  }
+
   return (
-    <div className="App">
-      <h1>ReactJs + Firebase</h1>
+    <div>
+      <h1>ReactJs + Firebase</h1> <br />
+
+      <div className="container">
+        <label>Titulo: </label>
+        <textarea type="text" value={titulo} onChange={ (e) => setTitulo(e.target.value)} />
+
+        <label>Autor: </label>
+        <input type="text" value={autor} onChange={ (e) => setAutor(e.target.value) }  />
+
+        <button onClick={ handleAdd }>Cadastrar</button>
+      </div>
+      
     </div>
   );
 }
