019f5a8bd9a73ec857249c281f091d48a0b03e19
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-07 20:21:19 -0300

    Permite logar obtendo e exibindo demais dados do usuário recém autenticado.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
   const [cargo, setCargo] = useState('');
   const [nome, setNome] = useState(''); 
 
+  const [user, setUser] = useState({});
+
   async function novoUsuario(){
     await firebase.auth().createUserWithEmailAndPassword(email, senha)
     .then( async (value)=>{
@@ -44,6 +46,28 @@ function App() {
     await firebase.auth().signOut();
   }
 
+  async function login(){
+    await firebase.auth().signInWithEmailAndPassword(email, senha)
+    .then(async (value)=>{
+      await firebase.firestore().collection('users')
+      .doc(value.user.uid)
+      .get()
+      .then((snapshot)=>{
+        setUser({
+          nome: snapshot.data().nome,
+          cargo: snapshot.data().cargo,
+          status: snapshot.data().status,
+          email: value.user.email
+        });
+
+      })
+    })
+    .catch((error)=>{
+      console.log('ERRO AO LOGAR' + error)
+    })
+
+  }
+
   return (
     <div>
       <h1>ReactJs + Firebase</h1> <br />
@@ -61,9 +85,21 @@ function App() {
         <label>Senha</label>
         <input type="password" value={senha} onChange={ (e) => setSenha(e.target.value) } /> <br/> 
         
+        <button onClick={ login } >Fazer Login</button>
         <button onClick={ novoUsuario }>Cadastrar</button>
         <button onClick={ logout } >Sair da conta!</button>      
       </div>
+
+      <hr/> <br/>
+  
+      { Object.keys(user).length > 0 && (
+        <div>
+          <strong>Olá </strong> {user.nome} <br/>
+          <strong>Cargo: </strong> {user.cargo} <br/>
+          <strong>Email: </strong> {user.email} <br/>
+          <strong>Status: </strong> {user.status ? 'ATIVO' : 'DESATIVADO'} <br/>
+        </div>
+      )}
       
     </div>
   );
